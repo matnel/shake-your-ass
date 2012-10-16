5f58079c402ae97b21627190f51b26a30ad3f157
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-10-16 19:07:14 +0300

    End of the Accelometer-library

--- client/js/accelerometer.js
+++ client/js/accelerometer.js
@@ -1,11 +1,40 @@
 function Accelerometer() {
 
+	// init the track
+	this._init();
+
+	this._DELTA = 10;
+
+
 };
 
 Accelerometer.prototype._init = function() {
-	window.addEventListener('deviceorientation', this._handle, false);
+	var meter = this;
+	window.addEventListener('deviceorientation', function(data){ meter._handle( data ) }, false);
 };
 
-Accelerometer.prototype._handle = function() {
+
+Accelerometer.prototype._handle = function( event ) {
+
+	if( event.gamma < - this._DELTA ) {
+		this.left();
+	}
+
+	if( event.gamma > this._DELTA ) {
+		this.right();
+	}
+
+	if( event.beta > this._DELTA ) {
+		this.up();
+	}
+
+	if( event.beta < - this._DELTA  ) {
+		this.down();
+	}
 
 }
+
+Accelerometer.prototype.up = $.noop;
+Accelerometer.prototype.down = $.noop;
+Accelerometer.prototype.left = $.noop;
+Accelerometer.prototype.right = $.noop;
