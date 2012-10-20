4a640fcf50d5fe430984c2b40cdd914744e9cb15
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-10-20 17:08:50 +0300

    Rotate bubble colors

--- screen/js/demo.js
+++ screen/js/demo.js
@@ -1,13 +1,18 @@
 var _HEIGHT = $(window).height();
 var _WIDTH = $(window).width();
 
+var _COLORS = ['#0099CC', '#0099FF', '#00CCFF'];
+var _count = 0;
+
 function Bubble() {
 	
-	this.element = $('<div>', { class : 'bubble', html : 'Daa' } );
+	this.element = $('<div>', { class : 'bubble' } );
 
-	this.element.css( { left : Math.random() * _WIDTH , top : Math.random() * _HEIGHT } );
+	this.element.css( { left : Math.random() * _WIDTH , top : Math.random() * _HEIGHT, background : _COLORS[ _count % _COLORS.length ] } );
 
 	$('body').append( this.element );
+
+	_count++;
 
 }
 Bubble.prototype.move = function( data ) {
